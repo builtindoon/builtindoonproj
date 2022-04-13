17567aa9b3fdaef132361045e42378a530309030
kamboj <vinitkr.kamboj@gmail.com>
2022-04-13 00:12:11 -0400

    Modified the code to running state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./component/page.jsx/About";
-import Contact from "./component/Contact";
-import Home from "./component/Home";
-import Services from "./component/services";
-import SignUp from "./component/Sign Up";
-import Skills from "./component/skills";
-import Teams from "./component/teams";
-export default function App() {
-  return (
-    <BrowserRouter>
-     <Routes>
-        <Route path="home" element={<Home />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="about" element={<About />} />
-        <Route path="services" element={<Services />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="skills" element={<Skills />} />
-        <Route path="teams" element={<Teams />} />
-      </Routes>
-    </BrowserRouter>
-  );
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+import Home from './views/home';
+import About from './views/about';
+import Services from './views/services';
+import SignUp from './views/signup';
+import Contact from './views/contact';
+import Team from './component/page/teams';
+import Skills from './component/page/skills'
+import Navbar from './components/Navbar'
+  
+function App() {
+return (
+    <Router>
+    <Navbar />
+    <Routes>
+        <Route exact path='/' element={<Home />} />
+       <Route path='/about' element={<About/>} />
+        <Route path='/services' element={<Services/>} />
+        <Route path='/skills' element={<Skills/>} />
+        <Route path='/team' element={<Team/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+    </Routes>
+    </Router>
+);
 }
+  
+export default App;
+  
